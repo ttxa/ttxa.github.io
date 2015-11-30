f2deacdff7623633749196491c15c0d72ec34f80
ttxa <tteshe4ko@gmail.com>
2015-12-01 00:08:49 +0200

    lesson2

--- GoIT/JavaScript/home/lesson2/script.js
+++ GoIT/JavaScript/home/lesson2/script.js
@@ -1,18 +1,18 @@
 
 var array = [];
  
-for (var i = 0; i < 3; i++){
+for (var i = 0; i < 5; i++) {
 	 array[i] = prompt('Имена пользователей',0);
 }
 
 var userName = prompt('Вводим свое имя:', '');
 
-for (var i = 0; i < 3; ++i) {
-	if (array[i] == userName)  {
+for (var i = 0; i < array.length; ++i) {
+	 if (array[i] == userName)  {
 		alert(userName + ', Вы успешно вошли' ) ;
 	} else {
 		alert( 'Нет таких' );
 	}
-    console.log(array[i]);
+     console.log(array[i]);
 }
 
