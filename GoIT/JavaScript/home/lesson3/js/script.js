61133f9ea39c9268930b4d7c5c205b2612f53ffb
ttxa <tteshe4ko@gmail.com>
2015-12-08 22:45:43 +0200

    working

--- GoIT/JavaScript/home/lesson3/js/script.js
+++ GoIT/JavaScript/home/lesson3/js/script.js
@@ -1,27 +1,84 @@
+var app = {
+
+  createElement: function(params) {
+    var element = document.createElement(params.tagName);
+
+    if (params.inputType){
+      element.setAttribute('type', params.inputType);
+    }
+
+    if (params.className){
+      element.className = params.className;
+    }
+
+    if (params.content){
+      element.innerHTML = params.content;
+    }
+
+    if (params.parentElement){
+      params.parentElement.appendChild(element);
+    }
+
+    return element;
+  },
+
+  generateQuestions: function(questionsAmount, answersAmount) {
+
+    for (var i = 0; i < questionsAmount; i++) {
+
+      this.createElement({
+        tagName: 'h2',
+        content: (i + 1) + '.' + 'Вопрос №' + (i + 1),
+        parentElement: form
+      });
+
+      for (var j = 0; j < answersAmount; j++) {
+
+        var label = this.createElement({
+          tagName: 'label',
+          content: 'Вариант ответа №' + (j + 1),
+          parentElement: form
+        });
+
+        var checkbox = this.createElement({
+          tagName: 'input',
+          inputType: 'checkbox'
+        });
+
+        label.insertAdjacentElement('afterBegin', checkbox);
+      }
+
+    };
+
+  }
+
+}
 
 
-
-
-
-
-
-var element = document.createElement('div');
-element.classList.add('wrapper');
-element.innerHTML = 'im  ';
 
 var body = document.querySelector('body');
-body.appendChild(element);
 
-var checkbox = document.createElement('input');
-checkbox.type = "checkbox";
-checkbox.name = "name";
-checkbox.value = "value";
-checkbox.id = "id";
 
-var label = document.createElement('label')
-label.htmlFor = "id";
-label.appendChild(document.createTextNode('Вариант ответа №1'));
+app.createElement({
+  tagName: 'h1',
+  content: 'Тест по программированию',
+  parentElement: body
+});
 
-body.appendChild(checkbox);
-body.appendChild(label);
+var form = app.createElement({
+  tagName: 'form',
+  parentElement: body
+});
+
+app.generateQuestions(3, 3);
+
+app.createElement({
+  tagName: 'input',
+  inputType: 'submit',
+  content: 'Проверить мои результаты',
+  className: 'btn btn-info',
+  parentElement: form
+});
+
+
 
